3484338f871dedbc6e37d2d77a533d05ebc3fcd0
fundamental-group <emathstutorials@gmail.com>
2023-05-08 13:26:02 -0700

    YS Trick Head

---
+++ YS Sketch Head/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Platform Object")]
+    public GameObject platform;
+
+    [Header("Game Over UI Canvas Object")]
+    public GameObject gameOverCanvas;
+
+    float pos = 0;
+
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+    }
+
+    void Start()
+    {
+        for(int i = 0; i < 1000; i++)
+        {
+            SpawnPlatforms();
+        }
+    }
+    void Update()
+    {
+
+    }
+    void SpawnPlatforms()
+    {
+        Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
+        pos += 2.5f;
+    }
+}
